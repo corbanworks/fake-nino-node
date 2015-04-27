1893d33351307954ef27bc8f65ed2af5f4a0880a
Jacob Allred <jacob.allred@corbanworks.com>
2015-04-27 18:10:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# fake-ssn-node
+
+This is a clone of [**fng-nino-tools**](https://github.com/corbanworks/fng-nino-tools) just in node.js
+
+A small library providing the ability to validate a UK national insurance number, and generate a fake national insurance number for testing your application.
+
+## Installation
+
+```bash
+  npm install fake-nino -g
+```
+
+## Usage
+
+```javascript
+  // Generate random NINO
+  var nino = require('fake-nino');
+  console.log(nino.generate());
+
+  // Validate NINO
+  if ( nino.validate('EC 55 50 80 A') ) {
+        console.log('valid');
+  } else {
+        console.log('invalid');
+  }
+```
+
+## Contributing
+
+If you find a bug or are willing to add some enhancement, pull requests are very welcome.
+
+## Release History
+
+* 0.0.1 Initial release
+
+## Legal
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
---
+++ index.js
@@ -0,0 +1,135 @@
+// Create a handy array function
+Array.prototype.in_array = function ( obj ) {
+    var len = this.length;
+    for ( var x = 0 ; x <= len ; x++ ) {
+        if ( this[x] == obj ) return true;
+    }
+    return false;
+}
+
+// Setup our groups
+var group1 = '';
+var group2 = '';
+var group3 = '';
+var group4 = '';
+var group5 = '';
+var randomArrayIndex = '';
+
+// Some combinations are not allowed
+var notAllowed = new Array(
+    'GB',
+    'BG',
+    'NK',
+    'KN',
+    'TN',
+    'NT',
+    'ZZ'
+);
+
+// Some letters can't be first and some can't be second
+var firstAllowed = new Array(
+    'A',
+    'B',
+    'C',
+    'E',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'R',
+    'S',
+    'T',
+    'W',
+    'X',
+    'Y',
+    'Z'
+);
+
+var secondAllowed = new Array(
+    'A',
+    'B',
+    'C',
+    'E',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'P',
+    'R',
+    'S',
+    'T',
+    'W',
+    'X',
+    'Y',
+    'Z'
+);
+
+// Only certain characters can show up last
+var lastAllowed = new Array(
+    'A',
+    'B',
+    'C',
+    'D',
+    ''
+);
+
+exports.generate = function (style) {
+    // Set a default value for style and make sure it is valid
+    var style = (style == null || (style !== 1 && style !== 2)) ? 1 : style;
+
+    // Generate group1
+    while( group1 == '' || notAllowed.in_array(group1) ) {
+        // First letter
+        randomArrayIndex = Math.floor(Math.random() * firstAllowed.length);
+        group1 = firstAllowed[ randomArrayIndex ];
+    
+        // Second letter
+        randomArrayIndex = Math.floor(Math.random() * secondAllowed.length);
+        group1 = group1 + secondAllowed[ randomArrayIndex ];
+    }
+
+    // Generate group2
+    group2 = ''+Math.floor(Math.random()*10)+Math.floor(Math.random()*10);
+
+    // Generate group3
+    group3 = ''+Math.floor(Math.random()*10)+Math.floor(Math.random()*10);
+
+    // Generate group4
+    group4 = ''+Math.floor(Math.random()*10)+Math.floor(Math.random()*10);
+
+    // Generate group5
+    randomArrayIndex = Math.floor(Math.random() * lastAllowed.length);
+    group5 = lastAllowed[ randomArrayIndex ];
+
+    if( style == 1 ){
+        var nino = group1+group2+group3+group4+group5;
+    } else {
+        var nino = group1+" "+group2+" "+group3+" "+group4;
+        if( group5 != '' ){
+            nino = nino + " " + group5;
+        }
+    }
+    
+    return nino;
+}
+
+exports.validate = function (nino) {
+        var regex = /^[A-CEGHJ-PR-TW-Z][A-CEGHJ-NPR-TW-Z] ?[0-9]{2} ?[0-9]{2} ?[0-9]{2} ?[ABCD]?/;
+
+    if (nino.match(regex)) {
+        regex = /^(GB)|^(BG)|^(NK)|^(KN)|^(TN)|^(NT)|^(ZZ)/;
+        if ( !nino.match(regex) ) {
+            return true;
+        }
+    }
+
+    return false;
+}
